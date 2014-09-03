20385e6b01d1c913de8b2e650b7da80edd7a489d
Sebastien Bartoli <sbstnbrtl@gmail.com>
2014-09-03 10:58:45 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-eu-cookie
-=========
+# EU Cookie Directive Bar
+
+This very simple and lightweight javascript module aims to add a small top bar on your website to display a custom message to your visitors, in compliance with the European Union Directive about cookies and privacy.
+
+## Usage
+
+Upload the folder anywhere on the internet, but preferably on one of your webserver. Replace the options in index.html with your context. 
+
+Only options 4 through 7 will need to be changed : 
+
+- 4 : replace "//cdn.example.org" with the domain where the folder is uploaded. The "//" before the URL insure the content will be loaded asynchronously AND using http or https depending on what's available.
+- 5 : except if you have changed the structure of the folder, you shouldn't have to change this.
+- 6 : change "subdomain.example.org" with the domain or subdomain on which you want the cookie to be applied (it should probably be the domain on which you are loading the bar).
+- 7 : you can add another, optional 7th option, that many will probably use, in order to completely customise the displayed text. It can handle any HTML code, so have fun.
+
+Then, copy-paste the modified content of index.html anywhere on your website, and voila! You're done.
+
+## Dependencies
+
+The module is using jQuery 1.11.1 and jquery.cookie.js, both are loaded by the module without need for intervention. jQuery is loaded only if the hosting website isn't available.
---
+++ js/cookie-manager.js
@@ -0,0 +1,80 @@
+function main($){
+	$.getScript( window.cmCDN + '/js/jquery.cookie.js', function(){
+		if( typeof($.cookie('eu-cookie-directive')) === "undefined" ){
+			
+			var cbhtml = $(document.createElement('div')).attr('style', 'display:none').addClass('cookie-bar disappear').append($(document.createElement('div')).addClass('cookie-bar-content')).append($(document.createElement('i')).addClass('fi-x')).append($(document.createElement('div')).addClass('clear'));
+			
+			var csshtml = $(document.createElement('link')).attr({rel: 'stylesheet', type: 'text/css', href: window.cmCDN + '/css/cookie-manager.css'});
+
+			var ficsshtml = $(document.createElement('link')).attr({rel: 'stylesheet', type: 'text/css', href: window.cmCDN + '/css/foundation-icons.css'});
+			$("head").append(csshtml).append(ficsshtml);
+			$(document).scroll(function(){
+				evalScrollPosition($);
+			});
+			
+			$(document).on('click', '.cookie-bar i, a', function(e){		
+				if($(e.target).is('i')){			
+					$('.cookie-bar').addClass('disappear');	
+				}		
+				$.cookie('eu-cookie-directive', 'accepted', {expires: 365, path: '/', domain: window.cmDomain});
+			});
+
+			if( typeof(window.cmTextContent) === "undefined" ){
+				window.cmTextContent = "By continuing your visit to this site, you accept the use of cookies, including for audience measurement and content sharing on social networks <a href='/privacy#use-of-cookies'>Read more and manage these settings.</a>" ;
+			}
+
+			$(cbhtml).find('.cookie-bar-content').html(window.cmTextContent);
+			
+			$("body").prepend(cbhtml);
+			evalScrollPosition($);
+			var tol = (loadjQuery == 'yep')? 50 : 2000 ;
+			window.setTimeout(function(){
+				$('.cookie-bar').attr('style', '');
+				window.setTimeout(function(){
+					$('.cookie-bar').removeClass('disappear');
+				}, 500);
+			}, tol);
+		} else {
+			console.warn('already accepted the cookies for this domain, no need to re-display the cookie bar');
+		}
+	});
+}
+
+function evalScrollPosition($){
+	if($(document).scrollTop() > $('.cookie-bar').outerHeight(true) && !$('.cookie-bar').hasClass('cfixed')){	
+		$('.cookie-bar').addClass('cfixed');
+	}
+	
+	if($(document).scrollTop() <= $('.cookie-bar').outerHeight(true) && $('.cookie-bar').hasClass('cfixed')){		
+		$('.cookie-bar').removeClass('cfixed');	
+	}
+}
+
+function loadDependencies(){
+	window.clearTimeout(loadjQuery);
+	console.log("loading jQuery since the host website doesn't seems to have it loaded");
+	var jqhtml = document.createElement('script');
+	jqhtml.async = 1 ;
+	jqhtml.src = window.cmCDN + '/js/jquery-1.11.1.min.js' ;
+	var el = document.getElementsByTagName('script')[0];
+	el.parentNode.insertBefore(jqhtml, el);
+}
+
+function startScript(){
+	if(typeof(window.jQuery) === "function"){
+		window.clearTimeout(checkjQuery);
+		window.clearTimeout(loadjQuery);
+		delete loadjQuery ;
+		delete checkjQuery ;
+		loadjQuery = 'yep' ;
+		main(jQuery);
+	} else {
+		if( loadjQuery != 'yep' ){
+			checkjQuery = window.setTimeout(startScript, 50);
+		}	
+	}
+}
+
+var checkjQuery, loadjQuery;
+loadjQuery = window.setTimeout(loadDependencies, 7000);
+startScript();
